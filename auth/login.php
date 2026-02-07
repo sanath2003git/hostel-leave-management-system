9cad2d0742b6e6094a2cfe0809aedce3696d106b
Sanath <sanathsreekumar@gmail.com>
2026-02-08 04:08:50 +0530

    Add login page UI (HTML + CSS)

---
+++ auth/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login | Hostel Leave Management</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+<div class="login-container">
+    <h2>Hostel Leave Management</h2>
+
+    <form>
+        <label>Username</label>
+        <input type="text" placeholder="Enter username">
+
+        <label>Password</label>
+        <input type="password" placeholder="Enter password">
+
+        <button type="submit">Login</button>
+    </form>
+</div>
+
+</body>
+</html>
